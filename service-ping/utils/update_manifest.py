7104b03a5047a333e5bfb06ff717d12cca143e96
Eli Haims <elihaims@gmail.com>
2016-06-23 01:47:47 -0700

    ping: Add ability to parse IPv6 addr for manifest

--- service-ping/utils/update_manifest.py
+++ service-ping/utils/update_manifest.py
@@ -1,16 +1,19 @@
 import os
-
+import re
 import yaml
 
 manifest_path = '/usr/src/app/manifest.yaml'
-zt_ip, port = os.environ['PAYMENT_SERVER_IP'].replace('https://', '').replace('http://', '').split(':')
+ipv6_pattern = re.compile(r"""\[(?P<ipv6_addr>(\w{4}:){7}\w{4})\]:(?P<port>\d*)$""")
+matches = ipv6_pattern.search(os.environ['PAYMENT_SERVER_IP'])
+zt_ip = matches.group('ipv6_addr')
+port = matches.group('port')
 
 with open(manifest_path, "r") as f:
     manifest_json = yaml.load(f)
 
 service = os.environ['SERVICE']
 manifest_json["basePath"] = "/%s" % service
-manifest_json["host"] = "%s:%s" % (zt_ip, port)
+manifest_json["host"] = "[%s]:%s" % (zt_ip, port)
 try:
     manifest_json["info"]["x-21-quick-buy"] = manifest_json["info"]["x-21-quick-buy"] % (zt_ip, port, service)
 except:
